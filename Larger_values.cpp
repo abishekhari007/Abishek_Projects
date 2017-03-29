a35e2e192da49e982c1b188b5427117b56fcdc56
Abishek Hari <abishekhari007@gmail.com>
2017-03-29 10:59:20 +0530

    Create Larger_values.cpp

---
+++ Larger_values.cpp
@@ -0,0 +1,14 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main() {
+    	int a,b,c;
+    	cin>>a>>b>>c;
+    	if(a>b)
+    	cout<<"A GREATER";
+    	else if(b>c)
+    	cout<<"B GREATER";
+    	else
+    	cout<<"C GREATER";
+    	return 0;
+    }
