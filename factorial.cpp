b8fc002861d42962b62396bee9dfe0a2a9821540
Abishek Hari <abishekhari007@gmail.com>
2017-03-30 16:11:10 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,12 +1,19 @@
-    #include <iostream>
+      #include <iostream>
     using namespace std;
      
     int main() {
     	int i,n,fact=1;
     	cin>>n;
+    	if(n<0)
+    	{
+    		cout<<"No factorial";
+    	}
+    	else
+    	{
     	for(i=1;i<=n;i++)
     	{
     		fact*=i;
+    	}
     	}
     	cout<<fact;
     	return 0;
